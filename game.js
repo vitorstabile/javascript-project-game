1bc6b7405c8e2a6db277edfd3996ab69deb133ac
Vitor Garcia <vitor.stabile@hotmail.com>
2021-03-19 15:23:25 -0300

    Take the level number by url search

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
 function gameBegin(){
 
-    var url = window.location;
+    var url = window.location.search;
 
-    alert(url);
+    var game_level = url.replace("?", "");
+
+    alert(game_level);
+
+    // 1 - Easy - 120 secs
+
+    // 2 - Normal - 60 secs
+
+    // 3 - Hard - 30 secs
 
 }
